7b07a4bb686101eaef5d0fc183ff416bfadc0560
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-03 16:59:40 +0300

    Task 07: Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """Class to perform Basic Authentication."""
-from api.v1.auth.auth import Auth
+from .auth import Auth
 
 
 class BasicAuth(Auth):
     """Inherit from Auth."""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Return the Base64 part of the Authorization header."""
+        if authorization_header:
+            if isinstance(authorization_header, str):
+                if authorization_header.startswith('Basic '):
+                    y = authorization_header.split(' ')
+                    y = str(y[1])
+                    return y
+        return None
