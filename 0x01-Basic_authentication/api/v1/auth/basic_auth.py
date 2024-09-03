06397048c137e8d789bd5cbce4536cd5f9d1e0bb
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-03 17:49:48 +0300

    Task 08: Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Class to perform Basic Authentication."""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,15 @@ class BasicAuth(Auth):
                     y = str(y[1])
                     return y
         return None
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str)-> str:
+        """Return the decoded value of a Base64 string."""
+        if base64_authorization_header:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    x = base64.b64decode(base64_authorization_header,
+                                         validate=True)
+                except Exception:
+                    return None
+                return x.decode('utf-8')
