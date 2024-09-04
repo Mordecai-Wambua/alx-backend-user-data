2a881ffe5a8eea2c9915444e8d6b486d551c6104
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-04 11:47:58 +0300

    Task 12: Basic - Allow password with ":"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,7 +40,7 @@ class BasicAuth(Auth):
             if isinstance(decoded_base64_authorization_header, str):
                 if decoded_base64_authorization_header.__contains__(':'):
                     return tuple(
-                        decoded_base64_authorization_header.split(':'))
+                        decoded_base64_authorization_header.split(':', 1))
         return (None, None)
 
     def user_object_from_credentials(self, user_email: str,
