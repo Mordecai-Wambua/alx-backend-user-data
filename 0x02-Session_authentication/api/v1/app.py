630b558370fb2ff61cc4ccfecbb7d5aaf2f2235a
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-05 10:35:54 +0300

    Task 05: Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,9 +31,10 @@ def filtering():
     if auth is None:
         pass
     exclusions = ['/api/v1/status/', '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/']
+                  '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, exclusions):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None\
+              and auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
