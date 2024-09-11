84f56ea421b3a07808d55af8265961ff492d17b8
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-11 11:20:44 +0300

    Task 09 & 10: Generate UUIDs & Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -36,3 +37,17 @@ class Auth:
             return False
         except Exception:
             return False
+
+    def _generate_uuid(self):
+        """Return a string representation of a new uuid."""
+        return str(uuid.uuid4())
+
+    def create_session(self, email: str):
+        """Generates an uuid and stores it as the user's session_id."""
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = self._generate_uuid()
+            self._db.update_user(user.id, session_id=uuid)
+            return user.session_id
+        except Exception:
+            return None
