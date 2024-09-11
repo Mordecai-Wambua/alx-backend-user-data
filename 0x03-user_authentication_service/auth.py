67c92ee1d71a85b0db3400e9f5a59f6ad1d7b474
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-11 11:04:45 +0300

    Task 08: Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,3 +26,13 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Checks if the user credentials are valid."""
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+            return False
+        except Exception:
+            return False
