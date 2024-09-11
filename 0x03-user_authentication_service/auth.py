e3d8d810cd7e934f1539c7afc2660db9b6001f52
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-09-11 13:17:05 +0300

    Task 16: (Corrections)

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,13 +72,13 @@ class Auth:
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate a reset password token."""
-        user = self._db.find_user_by(email=email)
-        if user:
+        try:
+            user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
-        else:
-            raise ValueError()
+        except NoResultFound:
+            raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Handles user password changes."""
